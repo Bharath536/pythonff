feac36c2c45430048543df3f0824fab3e39ffbaa
Bharath <youremail@gmail.com>
2026-06-17 03:38:30 -0700

    added day 3 tasks

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1=float(input("enter the frist number:"))
+num2=float(input("enter the secod number:"))
+print("addition:";num1+num2)
+print("subraction:";num1-num2)
+print("multiplication:";num1*num2)
+print("division:";num1/num2)
+print("modules:";num1%num2)
+print("power:";num1**nm2)
+
